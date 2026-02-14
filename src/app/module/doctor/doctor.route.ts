dac3a9e932742161db534719fe4f3f314b80b65a
Rizbi ahmmad <contact.rizbi123@gmail.com>
2026-02-15 02:24:27 +0600

    Adding create doctor,get,update,delete doctor in modules

---
+++ src/app/module/doctor/doctor.route.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+import { DoctorController } from "./doctor.controller";
+
+const router = Router();
+router.get("/", DoctorController.getAllDoctors);
+
+router.get(
+  "/:id",
+  DoctorController.getDoctorById,
+);
+router.patch("/:id", DoctorController.updateDoctor);
+router.delete(
+  "/:id",
+  DoctorController.deleteDoctor,
+);
+
+export const doctorRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { SpecialtyRouter } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.router";
+import { userRoutes } from "../module/user/user.route";
+import { doctorRoutes } from "../module/doctor/doctor.route";
 
-const router =Router()
+const router = Router();
 
-router.use("/auth",AuthRoutes)
-router.use("/specialties", SpecialtyRouter)
+router.use("/auth", AuthRoutes);
+router.use("/specialties", SpecialtyRouter);
+router.use("/users", userRoutes);
+router.use("/doctors", doctorRoutes);
 
-export const IndexRoutes=router
+export const IndexRoutes = router;
