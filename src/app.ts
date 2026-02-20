0fccaa0be371e24712b35e3bc134fb347b95a2bc
Rizbi ahmmad <contact.rizbi123@gmail.com>
2026-02-21 01:45:48 +0600

    Adding Google login

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,23 @@ import { notFound } from "./app/middleware/notFound";
 import cookieParser from "cookie-parser";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./app/lib/auth";
+import { envVars } from "./app/config/env";
+import path from "path";
+import qs from "qs";
+import cors from "cors";
 
 const app: Application = express();
+app.set("query parser", (str : string) => qs.parse(str));
+
+app.set("view engine", "ejs");
+app.set("views",path.resolve(process.cwd(), `src/app/templates`) )
+
+app.use(cors({
+    origin : [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
+    credentials : true,
+    methods : ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders : ["Content-Type", "Authorization"]
+}))
 
 app.use("/api/auth", toNodeHandler(auth))
 
