18679fe15c8ecb18d2fe2d402b23a37b803d91f6
Rizbi ahmmad <contact.rizbi123@gmail.com>
2026-02-27 02:07:35 +0600

    adding query builders and schedule module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { AuthRoutes } from "../module/auth/auth.router";
 import { userRoutes } from "../module/user/user.route";
-import { doctorRoutes } from "../module/doctor/doctor.route";
+
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
+import { DoctorRoutes } from "../module/doctor/doctor.route";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRoutes);
 router.use("/users", userRoutes);
-router.use("/doctors", doctorRoutes);
+router.use("/doctors", DoctorRoutes);
 
 export const IndexRoutes = router;
